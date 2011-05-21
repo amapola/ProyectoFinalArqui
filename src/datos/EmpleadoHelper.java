6ec2cff5ecefeff18e1002eb2a5ccd1ebcaf6903
amapola <amapola-s@hotmail.com>
2011-05-21 01:36:10 -0400

    creacion de clase

---
+++ src/datos/EmpleadoHelper.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package datos;
+
+import java.util.ArrayList;
+import modelo.*;
+
+/**
+ *
+ * @author HP
+ */
+public class EmpleadoHelper {
+
+    public ArrayList<Venta> getVentasDeEmpleado(String id)
+    {
+        ArrayList<Venta> ventas = new ArrayList<Venta>();
+        return ventas;
+    }
+
+}
