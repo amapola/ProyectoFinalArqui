c44906b28c63ef1fc16fc748ae9fd9262ceca2e7
amapola <amapola-s@hotmail.com>
2011-05-21 01:37:52 -0400

    creacion de clases

---
+++ src/modelo/main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo;
+
+/**
+ *
+ * @author HP
+ */
+public class main {
+    public void main(String[] args)
+    {
+
+    }
+
+}
